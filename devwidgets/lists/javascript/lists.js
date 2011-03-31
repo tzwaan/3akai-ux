6dcda9dd96600db9af70f82372257f275516a762
Chris Roby <christopher.roby@gmail.com>
2011-03-31 13:12:07 -0700

    NOJIRA updating the lists widget and adding in the templates

---
+++ devwidgets/lists/javascript/lists.js
@@ -0,0 +1,252 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+require(["jquery", "sakai/sakai.api.core", "/devwidgets/lists/listsconfig.js"], function($, sakai) {
+
+    /**
+     * @name sakai.lists
+     *
+     * @class lists
+     *
+     * @description
+     * Initialize the lists widget
+     *
+     * @version 0.0.1
+     * @param {String} tuid Unique id of the widget
+     * @param {Boolean} showSettings Show the settings of the widget or not
+     */
+    sakai_global.lists = function(tuid, showSettings) {
+
+        // Dom identifiers
+        var $rootel = $("#" + tuid);
+        var $listsSettings = $("#lists_settings", $rootel);
+        var $listsMain = $("#lists_main", $rootel);
+
+        var $listsSaveSettingsButton = $("#lists_save", $rootel);
+        var $listsCancelSettingsButton = $("#lists_cancel", $rootel);
+
+        var $listsOfLists = $("#lists_list_of_lists", $rootel);
+        var $listsTemplate = $("#lists_list_edit_template", $rootel);
+        var $listsDisplayTemplate = $("#lists_list_display_template", $rootel);
+
+        var $listSelect = $(".list_select", $rootel);
+        var $listTitle = $("#lists_title", $rootel);
+
+        var widgetData = {};
+        var thisList = {};
+
+        ////////////////////
+        // Main functions //
+        ////////////////////
+
+        var loadWidget = function(callback) {
+            sakai.api.Widgets.loadWidgetData(tuid, function(success, data) {
+                if (success) {
+                    widgetData = data;
+                }
+                if ($.isFunction(callback)) {
+                    callback();
+                }
+            });
+        };
+
+        var showLists = function() {
+            $listsMain.show();
+            if (widgetData.title) { // change the title of the widget
+                if (!sakai.api.Widgets.isOnDashboard(tuid)) {
+                    $listTitle.show();
+                    $listTitle.find("h1").text(widgetData.title);
+                    $(".lists_widget", $rootel).addClass("on_page");
+                } else {
+                    sakai.api.Widgets.changeWidgetTitle(tuid, widgetData.title);
+                }
+            }
+            if (widgetData.selections) {
+                sakai.api.Util.TemplateRenderer($listsDisplayTemplate, {"data": widgetData.selections}, $listsMain);
+            }
+        };
+
+        var getSaveData = function() {
+            if (($("select.list_final", $rootel).length > 0 && $("select.list_final option:selected", $rootel).length > 0) ||
+                 $(".lists_multi input[type=checkbox]:checked", $rootel).length > 0) {
+
+                widgetData.selections = [];
+                widgetData.parents = [];
+                $(".list_final", $rootel).parents(".list_container", $rootel).each(function(i,val) {
+                    if (i === 0) {
+                        widgetData.title = unescape($(val).attr("id"));
+                    }
+                    widgetData.parents[i] = unescape($(val).attr("id"));
+                });
+                if ($("select.list_final option:selected", $rootel).length > 0) {
+                    $("select.list_final option:selected", $rootel).each(function(i,val){
+                        var obj = {"title": unescape($(this).val())};
+                        if ($(this).attr("title")) {
+                            obj.loc = $(this).attr("title");
+                        }
+                        widgetData.selections.push(obj);
+                    });
+                } else {
+                    $(".lists_multi input[type=checkbox]:checked", $rootel).each(function(i,val){
+                        var obj = {"title": unescape($(this).val())};
+                        if ($(this).attr("title")) {
+                            obj.loc = $(this).attr("title");
+                        }
+                        widgetData.selections.push(obj);
+                    });
+                }
+                return true;
+            } else {
+                alert("Please make a selection before saving");
+                return false;
+            }
+        };
+
+        ////////////////////////
+        // Settings functions //
+        ////////////////////////
+
+        var renderInitialLists = function() {
+            setSelected();
+            $listsOfLists.html(sakai.api.Util.TemplateRenderer($listsTemplate, {"data":thisList, "hasLists": true, "parentLabel": "", "editParents": widgetData.editParents === false ? false : true}));
+            $(".list_select:not(.triggered):has(option:selected)", $rootel).addClass("triggered").trigger("change");
+        };
+
+        var setSelected = function() {
+            if (widgetData && widgetData.parents) {
+                // select the parents
+                for (var i=widgetData.parents.length-1, j=-1; i>j; i--) {
+                    var thisLabel = widgetData.parents[i];
+                    doSetSelectedRecursive(thisList, widgetData.parents, thisLabel);
+                }
+                // select the selections
+                $(widgetData.selections).each(function(i,val) {
+                    doSetSelectedRecursive(thisList, widgetData.parents, val.title);
+                });
+            }
+        };
+
+        var doSetSelectedRecursive = function(lists, parents, label) {
+            $(lists).each(function(i,val) {
+                if (label === val.Label || (val.title && label === val.title) || label === val) {
+                    if (label === val) {
+                        var tmp = val;
+                        val = {};
+                        val.title = tmp;
+                    }
+                    val.selected = true;
+                    lists[i] = val;
+                }
+            });
+            $(lists).each(function(i,val) {
+                if (val.list && $.inArray(val.Label, parents) !== -1) {
+                    doSetSelectedRecursive(val.list, parents, label);
+                }
+            });
+        };
+
+        /**
+         * getList
+         * Given a label, find the list associated with it inside of the current list
+         * Performs a breadth-first recursive search for the node
+         *
+         * @param {Object} lists The lists to start the search from
+         * @param {String} label The label to serach the lists for
+         */
+        var getList = function(lists, label) {
+            var ret = false;
+            $(lists).each(function(i,val) {
+                if (val.Label === label) {
+                    ret = val;
+                }
+            });
+            if (ret) {
+                return ret;
+            }
+            $(lists).each(function(i,val) {
+                if (val.list) {
+                    ret = getList(val.list, label);
+                }
+            });
+            if (ret) {
+                return ret;
+            }
+            return false;
+        };
+
+        ////////////////////
+        // Event Handlers //
+        ////////////////////
+        $listSelect.die("change");
+        $listSelect.live("change", function(e){
+            var id = $(this).attr("id").split("list_select_")[1];
+            var $parentDiv = $(this).parent("div");
+            var list = getList(thisList, unescape($(this).find("option:selected").val()));
+            if (list) {
+                if ($(".list_parent_" + id, $rootel).length) {
+                    // replace the current list display
+                    $(".list_parent_" + id, $rootel).replaceWith(sakai.api.Util.TemplateRenderer($listsTemplate, {"data":list, "parentLabel": unescape(id), "hasLists": list.list[0].list ? true : false, "editParents": widgetData.editParents === false ? false : true}));
+                } else {
+                    // append to the parent div for easy hiding
+                    $parentDiv.append(sakai.api.Util.TemplateRenderer($listsTemplate, {"data":list, "parentLabel": unescape(id), "hasLists": list.list[0].list ? true : false, "editParents": widgetData.editParents === false ? false : true}));
+                    $(".list_select:not(.triggered):has(option:selected)", $rootel).addClass("triggered").trigger("change"); // trigger change if there are more
+                    if ($(".list_select.list_final", $rootel).length) { // if the final list is available, select the selections
+                        $(widgetData.selections).each(function(i,val) {
+                            $(".list_final", $rootel).find("option[value='" + escape(val) + "']").attr("selected", "selected");
+                        });
+                    }
+                }
+            } else {
+                // remove the children
+                $(".list_parent_" + id, $rootel).remove();
+            }
+        });
+
+        /** Binds the lists save button*/
+        $listsSaveSettingsButton.bind("click", function(e) {
+            if (getSaveData()) {
+                sakai.api.Widgets.saveWidgetData(tuid, widgetData, function(success, data){
+                    sakai.api.Widgets.Container.informFinish(tuid, "lists");
+                });
+            }
+        });
+
+        $listsCancelSettingsButton.bind("click", function(e) {
+            sakai.api.Widgets.Container.informFinish(tuid, "lists");
+        });
+
+
+        var doInit = function(){
+            thisList = $.extend(true, {}, sakai_global.Lists);
+            loadWidget(function() {
+                if (showSettings) {
+                    renderInitialLists();
+                    var title = widgetData.title || "Lists";
+                    sakai.api.Widgets.changeWidgetTitle(tuid, title);
+                    $listsSettings.show();
+                } else {
+                    showLists();
+                }
+            });
+        };
+        doInit();
+
+    };
+
+    sakai.api.Widgets.widgetLoader.informOnLoad("lists");
+});
---
+++ devwidgets/lists/listsconfig.js
@@ -0,0 +1,170 @@
+sakai_global.Lists = {
+    "Label": "School",
+    "list": [
+    {
+        "Label": "Wagner",
+        "list": [
+        {
+            "Label": "Program",
+            "multiple": false,
+            "list": [
+                {
+                    "title":"MPA: PNP",
+                    "loc": "http://sakaiproject.org/product-overview"
+                },
+                {
+                    "title":"MPA: HPAM",
+                    "loc": "https://atlas.nyu.edu/~mpa--hpam"
+                },
+                {
+                    "title":"MUP",
+                    "loc": "http://sakaiproject.org/community-overview"
+                },
+                {
+                    "title":"EMPA",
+                    "loc": "http://sakaiproject.org/try-sakai-overview"
+                },
+                {
+                    "title":"EMPA: Nurse Leaders",
+                    "loc": "http://sakaiproject.org/news-events"
+                },
+                {
+                    "title":"PhD",
+                    "loc": "http://sakaiproject.org/contact"
+                }
+            ]
+        },
+        {
+            "Label": "Specialization",
+            "multiple": true,
+            "list": [
+            "Finance",
+            "International",
+            "Management",
+            "Policy",
+            "Environment, Infrastructure, and Transportation",
+            "Economic Development and Housing Policy",
+            "International Planning"
+            ]
+        },
+        {
+            "Label": "Enrollment Status",
+            "multiple": false,
+            "list": [
+            "Full-Time",
+            "Part-Time"
+            ]
+        },
+        {
+            "Label": "Graduation Date",
+            "multiple": false,
+            "list": [
+            "January 2011",
+            "May 2011",
+            "September 2011",
+            "January 2012",
+            "May 2012",
+            "September 2012",
+            "January 2013",
+            "May 2013",
+            "September 2013",
+            "January 2014",
+            "May 2014",
+            "September 2014",
+            "January 2015",
+            "May 2015",
+            "September 2015"
+            ]
+        },
+        {
+            "Label": "Interest Area(s)",
+            "multiple": true,
+            "list": [
+            "Aging",
+            "Arts and Culture",
+            "Children and Families",
+            "Civil Rights/Human Rights",
+            "Coalition Building",
+            "Community Development",
+            "Corporate Accountability",
+            "Identity and Diversity",
+            "Economic Development",
+            "Education",
+            "Elections",
+            "Environment",
+            "Finance",
+            "Food & Nutrition",
+            "Global Health",
+            "Governance",
+            "Health",
+            "Housing",
+            "Immigration",
+            "Infrastructure",
+            "International Development",
+            "Justice System",
+            "Labor and Employment",
+            "Leadership Development",
+            "Local and/or State Government",
+            "Microfinance",
+            "Nonprofit Management",
+            "NYC Government",
+            "Organizational Development",
+            "Philanthropy",
+            "Politics",
+            "Poverty/Hunger",
+            "Public Policy",
+            "Region - Africa: North",
+            "Region - Africa: Sub-Saharan",
+            "Region - Asia: Central",
+            "Region - Asia: East",
+            "Region - Asia: South",
+            "Region - Asia: Southeast",
+            "Region - Central America",
+            "Region - Europe: Eastern",
+            "Region - Europe: Western",
+            "Region - Former Soviet Republics",
+            "Region - Middle East",
+            "Region - North America",
+            "Region - Russia",
+            "Region - South America",
+            "Region - United States",
+            "Social Entrepreneurship",
+            "Transportation",
+            "Urban Planning",
+            "US Federal Government",
+            "US National Politics",
+            "Water"
+            ]
+        },
+        {
+            "Label": "Student Groups",
+            "multiple": true,
+            "list": [
+            "Wagner Student Association (WSA)",
+            "Alliance of Latin American Students (ALAS)",
+            "Asian Pacific American Student Alliance (APASA)",
+            "Black Student Alliance (BSA)",
+            "Jewish Wagner Professional Association (JeWPA)",
+            "Non Profit Network (NPN)",
+            "Stonewall Policy Alliance (SPA)",
+            "Students for Criminal Justice Reform (SCJR)",
+            "Students Exploring Arts and Culture (SNEAC)",
+            "Students for Innovation (BRIDGE)",
+            "Urban Planning Student Association (UPSA)",
+            "Wagner Review",
+            "Wagner Economic and Finance Association (WEFA)",
+            "Wagner Environmental Policy Association (WEPA)",
+            "Wagner Education Policy Studies Association (WEPSA)",
+            "Wagner Health Network (WHN)",
+            "Wagner International Student Society (WISS)",
+            "Wagner Policy Alliance (WPA)",
+            "Wagner Student Association for Africa(WSAFA)",
+            "Wagner Transportation Association (WTA)",
+            "Wagner Volunteer Corps (WVC)",
+            "Wagner Women's Caucus (WWC)"
+            ]
+        }
+        ]
+    }
+    ]
+};
