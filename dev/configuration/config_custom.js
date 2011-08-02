cc79189742658814dd95a4de49bb1644cc74493d
Carl Hall <carl@hallwaytech.com>
2011-08-02 10:04:20 -0400

    added external login description; styled external login button and container;
    Conflicts:
    
    	dev/configuration/config_custom.js

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -2,7 +2,20 @@ define(function() {
     // Insert custom configuration here
     var config = {
         // Custom CSS Files to load in
-        skinCSS : ["/dev/skins/nyu/nyu.skin.css"]
+        skinCSS : ["/dev/skins/nyu/nyu.skin.css"],
+
+        followLogoutRedirects : true,
+
+        Authentication : {
+            "allowInternalAccountCreation": false,
+            "internal": false,
+            "external": [{
+                label: "Proceed to Sign In",
+                url: "/system/sling/samlauth/login?resource=/dev/my_sakai.html",
+                description: "<p>Please <strong>Proceed to Sign In</strong> and enter your NetID and password.</p><p>You will be redirected back to Atlas upon successful login.</p>"
+            }],
+            "hideLoginOn": ["/dev", "/dev/index.html", "/dev/create_new_account.html"]
+        }
     };
     return config;
 });
